28b6c9ffce6f39de37bb0fc5d0235b215a523d3b
Rohan-Banerjee <38259703+Rohan-Banerjee@users.noreply.github.com>
2021-06-16 17:47:06 +0530

    Add files via upload

---
+++ ContentModeration/src/main/java/rohan/projects/ContentModeration/ContentModerationApplication.java
@@ -0,0 +1,13 @@
+package rohan.projects.ContentModeration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentModerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentModerationApplication.class, args);
+	}
+
+}
---
+++ ContentModeration/src/test/java/rohan/projects/ContentModeration/ContentModerationApplicationTests.java
@@ -0,0 +1,13 @@
+package rohan.projects.ContentModeration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentModerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
